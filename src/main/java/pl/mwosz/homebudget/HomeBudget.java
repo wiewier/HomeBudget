4f476c066087fea7e6363eba655800d57fe5c7a9
MichalW <wiewierski@o2.pl>
2018-09-15 15:10:06 +0200

    [add] - home.html, HomeBudget, HomeController

---
+++ src/main/java/pl/mwosz/homebudget/HomeBudget.java
@@ -0,0 +1,13 @@
+package pl.mwosz.homebudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeBudget {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(HomeBudget.class, args);
+    }
+}
